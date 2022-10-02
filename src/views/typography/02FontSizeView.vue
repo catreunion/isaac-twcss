9027bc75f22ea0a10d50a4e3cc0ff54d0f8c9d7b
Isaac Li <isaacli@mac.com>
2022-10-02 16:06:14 +0800

    version 1.0.13

--- src/views/typography/02FontSizeView.vue
+++ src/views/typography/02FontSizeView.vue
@@ -1,6 +1,19 @@
 <template>
   <main>
     <h1>Font Size</h1>
+    <h3>control the font size of an element</h3>
+    <p>`text-xs`</p>
+    <p>`text-sm`</p>
+    <p>`text-base`</p>
+    <p>`text-lg`</p>
+    <p>`text-xl` to `text-9xl`</p>
+    <p class="text-xs hover:text-9xl">The quick brown fox jumps over the lazy dog</p>
+    <p class="text-sm hover:text-9xl">The quick brown fox jumps over the lazy dog</p>
+    <p class="text-base hover:text-9xl">The quick brown fox jumps over the lazy dog</p>
+    <p class="text-lg hover:text-9xl">The quick brown fox jumps over the lazy dog</p>
+    <p class="text-xl hover:text-9xl">The quick brown fox jumps over the lazy dog</p>
+    <p class="text-2xl hover:text-9xl">The quick brown fox jumps over the lazy dog</p>
+    <p class="text-[14px] hover:text-9xl">The quick brown fox jumps over the lazy dog</p>
   </main>
 </template>
 
