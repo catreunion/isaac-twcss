0ec5a9b64b8f9d9615bd769b6c9161ddb51f07b4
Isaac Li <isaacli@mac.com>
2022-10-02 16:19:15 +0800

    version 1.0.14

--- src/views/typography/04FontStyleView.vue
+++ src/views/typography/04FontStyleView.vue
@@ -1,6 +1,11 @@
 <template>
   <main>
     <h1>Font Style</h1>
+    <h3>control the style of text</h3>
+    <p>`italic`</p>
+    <p>`not-italic`</p>
+    <p class="italic hover:not-italic">The quick brown fox jumps over the lazy dog</p>
+    <p class="not-italic hover:italic">The quick brown fox jumps over the lazy dog</p>
   </main>
 </template>
 
