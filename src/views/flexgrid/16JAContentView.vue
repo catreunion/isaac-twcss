423248954ac3dfe37f80138f591668e7a4b20359
Isaac Li <isaacli@mac.com>
2022-10-09 08:06:51 +0800

    version 1.0.37

--- src/views/flexgrid/16JAContentView.vue
+++ src/views/flexgrid/16JAContentView.vue
@@ -1,16 +1,15 @@
 <template>
   <main>
     <h1>Justify Content</h1>
-    <h2>control how items are positioned along the container's x axis</h2>
+    <h2>control how columns are positioned along the container's x axis</h2>
     <h3 class="font-mono">justify-start|justify-center|justify-end|</h3>
     <h3 class="font-mono">justify-between|justify-around|justify-evenly</h3>
-    <br />
 
-    <div class="grid-col-1 grid gap-8">
+    <div class="grid-col-1 grid gap-8 py-8">
       <div>
         <h3 class="font-mono">justify-start</h3>
-        <p>justify items against the start of the container’s x axis</p>
-        <div class="flex justify-start">
+        <p>pack columns against the start of the container’s x axis</p>
+        <div class="flex justify-start border-2 border-pink-500">
           <div class="w-20 border-2 border-indigo-500 p-2">File</div>
           <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
           <div class="w-20 border-2 border-indigo-500 p-2">View</div>
@@ -20,8 +19,8 @@
 
       <div>
         <h3 class="font-mono">justify-center</h3>
-        <p>justify items along the center of the container’s x axis</p>
-        <div class="flex justify-center">
+        <p>pack columns along the center of the container’s x axis</p>
+        <div class="flex justify-center border-2 border-pink-500">
           <div class="w-20 border-2 border-indigo-500 p-2">File</div>
           <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
           <div class="w-20 border-2 border-indigo-500 p-2">View</div>
@@ -31,8 +30,8 @@
 
       <div>
         <h3 class="font-mono">justify-end</h3>
-        <p>justify items against the end of the container’s x axis</p>
-        <div class="flex justify-end">
+        <p>pack columns against the end of the container’s x axis</p>
+        <div class="flex justify-end border-2 border-pink-500">
           <div class="w-20 border-2 border-indigo-500 p-2">File</div>
           <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
           <div class="w-20 border-2 border-indigo-500 p-2">View</div>
@@ -42,9 +41,9 @@
 
       <div>
         <h3 class="font-mono">justify-between</h3>
-        <p>justify items along the container’s x axis +</p>
+        <p>justify columns along the container’s x axis +</p>
         <p>equal amount of space between each item</p>
-        <div class="flex justify-between">
+        <div class="flex justify-between border-2 border-pink-500">
           <div class="w-20 border-2 border-indigo-500 p-2">File</div>
           <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
           <div class="w-20 border-2 border-indigo-500 p-2">View</div>
@@ -54,9 +53,9 @@
 
       <div>
         <h3 class="font-mono">justify-around</h3>
-        <p>justify items along the container’s x axis +</p>
+        <p>justify columns along the container’s x axis +</p>
         <p>equal amount of space on each side of each item</p>
-        <div class="flex justify-around">
+        <div class="flex justify-around border-2 border-pink-500">
           <div class="w-20 border-2 border-indigo-500 p-2">File</div>
           <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
           <div class="w-20 border-2 border-indigo-500 p-2">View</div>
@@ -66,9 +65,9 @@
 
       <div>
         <h3 class="font-mono">justify-evenly</h3>
-        <p>justify items along the container’s x axis +</p>
+        <p>justify columns along the container’s x axis +</p>
         <p>equal amount of space around each item</p>
-        <div class="flex justify-evenly">
+        <div class="flex justify-evenly border-2 border-pink-500">
           <div class="w-20 border-2 border-indigo-500 p-2">File</div>
           <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
           <div class="w-20 border-2 border-indigo-500 p-2">View</div>
@@ -76,15 +75,13 @@
         </div>
       </div>
     </div>
-    <br />
 
     <h1>Align Content</h1>
     <h2>control how rows are positioned along the container's y axis</h2>
     <h3 class="font-mono">content-start|content-center|content-end|</h3>
     <h3 class="font-mono">content-between|content-around|content-evenly</h3>
-    <br />
 
-    <div class="grid-col-1 grid gap-8">
+    <div class="grid-col-1 grid gap-8 py-8">
       <div>
         <h3 class="font-mono">content-start</h3>
         <p>pack rows against the start of the container’s y axis</p>
@@ -120,7 +117,7 @@
 
       <div>
         <h3 class="font-mono">content-between</h3>
-        <p>distribute rows in a container +</p>
+        <p>distribute rows along the container’s y axis +</p>
         <p>equal amount of space between each row</p>
         <div class="grid h-80 grid-cols-1 content-between border-2 border-pink-500">
           <div class="h-10 w-20 border-2 border-indigo-500 p-2">File</div>
@@ -132,7 +129,7 @@
 
       <div>
         <h3 class="font-mono">content-around</h3>
-        <p>distribute rows in a container +</p>
+        <p>distribute rows along the container’s y axis +</p>
         <p>equal amount of space around each side of each row</p>
         <div class="grid h-80 grid-cols-1 content-around border-2 border-pink-500">
           <div class="h-10 w-20 border-2 border-indigo-500 p-2">File</div>
@@ -144,7 +141,7 @@
 
       <div>
         <h3 class="font-mono">content-evenly</h3>
-        <p>distribute rows in a container +</p>
+        <p>distribute rows along the container’s y axis +</p>
         <p>equal amount of space around each row</p>
         <div class="grid h-80 grid-cols-1 content-evenly border-2 border-pink-500">
           <div class="h-10 w-20 border-2 border-indigo-500 p-2">File</div>
--- src/views/flexgrid/17JustifyItemsView.vue
+++ src/views/flexgrid/17JustifyItemsView.vue
@@ -1,64 +1,52 @@
 <template>
   <main>
-    <h1>Justify Content</h1>
-    <h2>control how items are positioned along the container's x axis</h2>
-    <h3 class="font-mono">justify-start|justify-center|justify-end|</h3>
-    <h3 class="font-mono">justify-between|justify-around|justify-evenly</h3>
-    <br />
+    <h1>Justify Items</h1>
+    <h2>control how grid items are aligned along their inline x axis</h2>
+    <h3 class="font-mono">justify-items-start|justify-items-end|</h3>
+    <h3 class="font-mono">justify-items-center|justify-items-stretch</h3>
 
-    <div class="grid-col-1 grid gap-8">
+    <div class="grid-col-1 grid gap-8 py-8">
       <div>
-        <h3 class="font-mono">justify-start</h3>
-        <p>justify items against the start of the container’s x axis</p>
-        <div class="flex justify-start">
-          <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-          <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-          <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-          <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+        <h3 class="font-mono">justify-items-start</h3>
+        <p>Align grid items against the start of their inline x axis</p>
+        <div class="grid grid-cols-3 justify-items-start border-2 border-pink-500">
+          <div class="border-2 border-indigo-500 p-2">File</div>
+          <div class="border-2 border-indigo-500 p-2">Edit</div>
+          <div class="border-2 border-indigo-500 p-2">View</div>
+          <div class="border-2 border-indigo-500 p-2">Help</div>
         </div>
       </div>
 
       <div>
-        <h3 class="font-mono">justify-between</h3>
-        <p>justify items along the container’s x axis +</p>
-        <p>equal amount of space between each item</p>
-        <div class="flex justify-between">
-          <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-          <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-          <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-          <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
-        </div>
-      </div>
-    </div>
-    <br />
-
-    <h1>Align Content</h1>
-    <h2>control how rows are positioned along the container's y axis</h2>
-    <h3 class="font-mono">content-start|content-center|content-end|</h3>
-    <h3 class="font-mono">content-between|content-around|content-evenly</h3>
-    <br />
-
-    <div class="grid-col-1 grid gap-8">
-      <div>
-        <h3 class="font-mono">content-start</h3>
-        <p>pack rows against the start of the container’s y axis</p>
-        <div class="grid h-80 grid-cols-1 content-start border-2 border-pink-500">
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">File</div>
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">Edit</div>
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">View</div>
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">Help</div>
+        <h3 class="font-mono">justify-items-end</h3>
+        <p>Align grid items against the end of their inline x axis</p>
+        <div class="grid grid-cols-3 justify-items-end border-2 border-pink-500">
+          <div class="border-2 border-indigo-500 p-2">File</div>
+          <div class="border-2 border-indigo-500 p-2">Edit</div>
+          <div class="border-2 border-indigo-500 p-2">View</div>
+          <div class="border-2 border-indigo-500 p-2">Help</div>
         </div>
       </div>
 
       <div>
-        <h3 class="font-mono">content-evenly</h3>
-        <p>distribute rows in a container +</p>
-        <p>equal amount of space around each row</p>
-        <div class="grid h-80 grid-cols-1 content-evenly border-2 border-pink-500">
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">File</div>
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">Edit</div>
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">View</div>
-          <div class="h-10 w-20 border-2 border-indigo-500 p-2">Help</div>
+        <h3 class="font-mono">justify-items-center</h3>
+        <p>Align grid items along their inline x axis</p>
+        <div class="grid grid-cols-3 justify-items-center border-2 border-pink-500">
+          <div class="border-2 border-indigo-500 p-2">File</div>
+          <div class="border-2 border-indigo-500 p-2">Edit</div>
+          <div class="border-2 border-indigo-500 p-2">View</div>
+          <div class="border-2 border-indigo-500 p-2">Help</div>
+        </div>
+      </div>
+
+      <div>
+        <h3 class="font-mono">justify-items-stretch</h3>
+        <p>stretch grid items along their inline x axis</p>
+        <div class="grid grid-cols-3 justify-items-stretch border-2 border-pink-500">
+          <div class="border-2 border-indigo-500 p-2">File</div>
+          <div class="border-2 border-indigo-500 p-2">Edit</div>
+          <div class="border-2 border-indigo-500 p-2">View</div>
+          <div class="border-2 border-indigo-500 p-2">Help</div>
         </div>
       </div>
     </div>
