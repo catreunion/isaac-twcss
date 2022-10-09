1978aeb30d61ae3d9671a175e6713bf50c546a9b
Isaac Li <isaacli@mac.com>
2022-10-09 20:33:19 +0800

    version 1.0.43

--- src/views/spacing/01PaddingView.vue
+++ src/views/spacing/01PaddingView.vue
@@ -2,7 +2,6 @@
   <main>
     <h1>Padding</h1>
     <h2>set an element's padding</h2>
-    <h3 class="font-mono">p{t|r|b|l}-{size}</h3>
     <h3 class="font-mono">size : p-px|px-px|py-px|pt-px|pr-px|pb-px|pl-px</h3>
     <h3 class="font-mono">size : 0|0.5|1|1.5|2|2.5|3|3.5|</h3>
     <h3 class="font-mono">size : 4|5|6|7|8|9|10|11|12|</h3>
@@ -22,17 +21,10 @@
       </div>
 
       <div>
-        <h3 class="font-mono">px-{size}</h3>
-        <p class="font-mono">add horizontal padding to an element</p>
+        <h3 class="font-mono">p{x|y}-{size}</h3>
+        <p class="font-mono">add horizontal|vertical padding to an element</p>
         <div class="flex justify-center">
           <div class="border-2 border-indigo-500 px-8">File</div>
-        </div>
-      </div>
-
-      <div>
-        <h3 class="font-mono">py-{size}</h3>
-        <p class="font-mono">add vertical padding to an element</p>
-        <div class="flex justify-center">
           <div class="border-2 border-indigo-500 py-8">File</div>
         </div>
       </div>
@@ -46,7 +38,7 @@
       </div>
 
       <div>
-        <h3 class="font-mono">arbitrary values</h3>
+        <h3>arbitrary values</h3>
         <div class="flex justify-center">
           <div class="border-2 border-indigo-500 p-[15px]">File</div>
         </div>
