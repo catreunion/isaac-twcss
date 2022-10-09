7d670b46d1b5fa1d5564bf0b09cc924d131179d0
Isaac Li <isaacli@mac.com>
2022-10-09 08:26:21 +0800

    version 1.0.38

---
+++ src/views/flexgrid/20test.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1>Align Items</h1>
+    <h2>control how items are aligned along their inline y axis</h2>
+    <h3 class="font-mono">items-start|items-end|items-center|</h3>
+    <h3 class="font-mono">items-baseline|items-stretch</h3>
+
+    <div class="grid-col-1 grid gap-8 py-8">
+      <div>
+        <h3 class="font-mono">items-start</h3>
+        <p>Align grid items against the start of their inline x axis</p>
+        <div class="flex items-start border-2 border-pink-500">
+          <div class="border-2 border-indigo-500 py-4">File</div>
+          <div class="border-2 border-indigo-500 py-12">Edit</div>
+          <div class="border-2 border-indigo-500 py-8">View</div>
+          <div class="border-2 border-indigo-500 py-10">Help</div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
