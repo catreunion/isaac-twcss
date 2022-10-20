7565e8dbcb3cc862a78706b01157349cff3ff3dc
Isaac Li <isaacli@mac.com>
2022-10-20 20:26:07 +0800

    version 1.0.86

--- src/views/flexgrid/03JAContentView.vue
+++ src/views/flexgrid/03JAContentView.vue
@@ -13,10 +13,10 @@
             <h5 class="font-mono">justify-start</h5>
             <p>pack columns at the start of the container’s x axis</p>
             <div class="flex justify-start border-2 border-pink-500 p-2">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
@@ -24,10 +24,10 @@
             <h5 class="font-mono">justify-center</h5>
             <p>pack columns at the center of the container’s x axis</p>
             <div class="flex justify-center border-2 border-pink-500 p-2">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
@@ -35,10 +35,10 @@
             <h5 class="font-mono">justify-end</h5>
             <p>pack columns at the end of the container’s x axis</p>
             <div class="flex justify-end border-2 border-pink-500 p-2">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
@@ -46,10 +46,10 @@
             <h5 class="font-mono">justify-between</h5>
             <p>equal amount of space between each item along the container’s x axis</p>
             <div class="flex justify-between border-2 border-pink-500 p-2">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
@@ -57,10 +57,10 @@
             <h5 class="font-mono">justify-around</h5>
             <p>equal amount of space on each side of each item along the container’s x axis</p>
             <div class="flex justify-around border-2 border-pink-500 p-2">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
@@ -68,10 +68,10 @@
             <h5 class="font-mono">justify-evenly</h5>
             <p>equal amount of space around each item along the container’s x axis</p>
             <div class="flex justify-evenly border-2 border-pink-500 p-2">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
         </div>
@@ -84,66 +84,66 @@
         <h3 class="font-mono">content-{y}</h3>
         <h4 class="font-mono">start|center|end|between|around|evenly</h4>
 
-        <div class="grid-col-1 grid gap-6 py-6">
+        <div class="grid grid-cols-1 gap-3 py-3">
           <div>
             <h5 class="font-mono">content-start</h5>
             <p>pack rows at the start of the container’s y axis</p>
-            <div class="grid-col-1 grid h-56 content-start border-2 border-pink-500">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+            <div class="grid h-40 grid-cols-3 content-start border-2 border-pink-500 p-2">
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
           <div>
             <h5 class="font-mono">content-center</h5>
             <p>pack rows at the center of the container’s y axis</p>
-            <div class="grid-col-1 grid h-56 content-center border-2 border-pink-500">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+            <div class="grid h-40 grid-cols-3 content-center border-2 border-pink-500 p-2">
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
           <div>
             <h5 class="font-mono">content-end</h5>
             <p>pack rows at the end of the container’s y axis</p>
-            <div class="grid-col-1 grid h-56 content-end border-2 border-pink-500">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+            <div class="grid h-40 grid-cols-3 content-end border-2 border-pink-500 p-2">
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
           <div>
             <h5 class="font-mono">content-between</h5>
             <p>equal amount of space between each row along the container’s y axis</p>
-            <div class="grid-col-1 grid h-56 content-between border-2 border-pink-500">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+            <div class="grid h-40 grid-cols-3 content-between border-2 border-pink-500 p-2">
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
           <div>
             <h5 class="font-mono">content-around</h5>
             <p>equal amount of space on each side of each row along the container’s y axis</p>
-            <div class="grid-col-1 grid h-56 content-around border-2 border-pink-500">
-              <div class="w-20 border-2 border-indigo-500 p-2">File</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">View</div>
-              <div class="w-20 border-2 border-indigo-500 p-2">Help</div>
+            <div class="grid h-40 grid-cols-3 content-around border-2 border-pink-500">
+              <div class="border-2 border-indigo-500 p-2">File</div>
+              <div class="border-2 border-indigo-500 p-2">Edit</div>
+              <div class="border-2 border-indigo-500 p-2">View</div>
+              <div class="border-2 border-indigo-500 p-2">Help</div>
             </div>
           </div>
 
           <div class="border-2 border-green-500 p-2">
             <h5 class="font-mono">content-evenly</h5>
             <p>equal amount of space around each row along the container’s y axis</p>
-            <div class="grid-col-1 grid h-56 content-evenly border-2 border-pink-500">
+            <div class="grid h-40 grid-cols-3 content-evenly border-2 border-pink-500">
               <div class="w-20 border-2 border-indigo-500 p-2">File</div>
               <div class="w-20 border-2 border-indigo-500 p-2">Edit</div>
               <div class="w-20 border-2 border-indigo-500 p-2">View</div>
