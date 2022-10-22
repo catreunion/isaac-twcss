26dcb942668daba519d5d7657121d4e917fa72bc
Isaac Li <isaacli@mac.com>
2022-10-22 10:45:43 +0800

    version 1.0.96

--- src/views/layout/02ContainerView.vue
+++ src/views/layout/02ContainerView.vue
@@ -110,6 +110,69 @@
         <h3 class="font-mono">flow-root|grid|inline-grid|</h3>
         <h3 class="font-mono">contents|list-item|hidden</h3>
       </div>
+
+      <!-- Floats -->
+      <div>
+        <h1>Floats</h1>
+        <h2>set the wrapping of content around an element</h2>
+        <h3 class="font-mono">float-right|left|none</h3>
+      </div>
+
+      <!-- Clear -->
+      <div>
+        <h1>Clear</h1>
+        <h2>set the wrapping of content around an element</h2>
+        <h3 class="font-mono">clear-left|right|both|none</h3>
+      </div>
+
+      <!-- Isolation -->
+      <div>
+        <h1>Isolation</h1>
+        <h2>control whether an element should explicitly create a new stacking context</h2>
+        <h3 class="font-mono">isolate|isolation-auto</h3>
+      </div>
+
+      <!-- Object Fit -->
+      <div>
+        <h1>Object Fit</h1>
+        <h2>control how a replaced element's content should be resized</h2>
+        <h3 class="font-mono">object-{contain|cover|fill|none|scale-down}</h3>
+
+        <div class="grid grid-cols-1 gap-3 border-2 border-pink-500 p-2 py-3">
+          <div class="border-2 border-indigo-500 p-2">
+            <h4 class="font-mono">object-contain</h4>
+            <p>resize an element’s content to stay contained within its container</p>
+            <img class="h-48 w-96 object-contain" src="/SLAM_DUNK.jpeg" alt="SLAM DUNK" />
+          </div>
+
+          <div class="border-2 border-indigo-500 p-2">
+            <h4 class="font-mono">object-cover</h4>
+            <p>resize an element’s content to cover its container</p>
+            <img class="h-48 w-96 object-cover" src="/SLAM_DUNK.jpeg" alt="SLAM DUNK" />
+          </div>
+
+          <div class="border-2 border-indigo-500 p-2">
+            <h4 class="font-mono">object-fill</h4>
+            <p>stretch an element’s content to fit its container</p>
+            <img class="h-48 w-96 object-fill" src="/SLAM_DUNK.jpeg" alt="SLAM DUNK" />
+          </div>
+
+          <div class="border-2 border-indigo-500 p-2">
+            <h4 class="font-mono">object-none</h4>
+            <p>display an element’s content at its original size ignoring the container size</p>
+            <img class="h-48 w-96 object-none" src="/SLAM_DUNK.jpeg" alt="SLAM DUNK" />
+          </div>
+
+          <div class="border-2 border-indigo-500 p-2">
+            <h4 class="font-mono">object-scale-down</h4>
+            <p>
+              display an element’s content at its original size but scale it down to fit its container if
+              necessary
+            </p>
+            <img class="h-48 w-96 object-scale-down" src="/SLAM_DUNK.jpeg" alt="SLAM DUNK" />
+          </div>
+        </div>
+      </div>
     </div>
   </main>
 </template>
